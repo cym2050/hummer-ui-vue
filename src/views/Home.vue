60ddd215ff03b101136563cf950e1cb2845af902
cym2050 <1390062269@qq.com>
2020-09-05 12:46:49 +0800

    home添加css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,6 @@
         <a>开始</a>
       </p>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -18,5 +17,25 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
