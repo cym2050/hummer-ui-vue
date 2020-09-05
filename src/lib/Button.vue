9ccdcdbc781600c653a3adc4a664db37d0ece66f
cym2050 <1390062269@qq.com>
2020-09-05 23:58:51 +0800

    Button组件基本完成

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,45 @@
 <template>
-  <button class="hummer-button" :class="{[`hummer-theme-${theme}`]: theme}">
+  <button class="hummer-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="hummer-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
-
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`hummer-theme-${theme}`]: theme,
+        [`hummer-size-${size}`]: size,
+	      [`hummer-level-${level}`]: level,
+      }
+    })
+    return { classes } 
+  }
 };
 </script>
 <style lang="scss">
@@ -20,6 +48,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .hummer-button {
   box-sizing: border-box;
   height: $h;
@@ -34,6 +64,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -61,9 +92,98 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
     &:hover,&:focus{
-      background: darken(white, 5%);;
+      background: darken(white, 5%);
     }
-
   }
+  
+  &.hummer-theme-button{
+    &.hummer-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.hummer-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+    &.hummer-theme-button {
+      &.hummer-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+      &.hummer-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+    &.hummer-theme-link {
+      &.hummer-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+    &.hummer-theme-text {
+      &.hummer-level-main {
+        color: $blue;
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+      &.hummer-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+    &.hummer-theme-button {
+      &[disabled] {
+        cursor: not-allowed;
+        color: $grey;
+        &:hover {
+          border-color: $grey;
+        }
+      }
+    }
+    &.hummer-theme-link, &.hummer-theme-text {
+      &[disabled] {
+        cursor: not-allowed;
+        color: $grey;
+      }
+    }
+  }
+  > .hummer-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: hummer-spin 1s infinite linear;
+  }
+}
+@keyframes hummer-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>
