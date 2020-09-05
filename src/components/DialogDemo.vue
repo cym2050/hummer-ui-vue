aadcc41eec8114722ff0b6968b62bc86303c0e03
cym2050 <1390062269@qq.com>
2020-09-06 00:10:06 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,12 @@
 <template>
-    <h2>Dialog</h2>
+    <h2>Dialog 示例</h2>
+    <Dialog><Dialog/>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: { Dialog }
+}
+</script>
+
