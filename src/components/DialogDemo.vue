455eb4d1b8844f91fc89c879fe238c62fc2dd619
cym2050 <1390062269@qq.com>
2020-09-06 15:54:08 +0800

    dialog组件支持visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,23 @@
 <template>
     <h2>Dialog 示例</h2>
-    <Dialog><Dialog/>
+    <h1>示例1</h1>
+    <Button @click="toggle">toggle</Button>  
+    <Dialog :visible="x"><Dialog/>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 export default {
-  components: { Dialog }
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    } 
+    return { x, toggle }
+  }
 }
 </script>
 
