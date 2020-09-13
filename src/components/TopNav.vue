942435715af575df04fc58e6d27d9e6db593dfb3
cym2050 <1390062269@qq.com>
2020-09-13 22:24:14 +0800

    修改文字、链接样式

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -23,7 +23,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #28d1c9;
 .topnav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,11 +21,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #28d1c9;
 .topBarAndBanner {
   background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(182,233,230,1) 100%);
 }
 
 .banner {
+  color: $color; 
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -35,13 +39,14 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
