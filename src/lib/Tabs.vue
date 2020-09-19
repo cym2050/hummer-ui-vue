b7b1338a1d76c59b0bdfa2dfd99bc4fb70f91be7
cym2050 <1390062269@qq.com>
2020-09-19 22:47:11 +0800

    tabs 更新

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>Tabs 组件</div>
+  <div>
+    Tabs 组件
+    <component :is="defaults[0]" /> 
+    <component :is="defaults[1]" /> 
+  </div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default()
+    console.log(defaults[0])
+
+    return { defaults }
+  },
+  // beforeCreate() {
+  //   console.log('beforeCreate');
+  // },
+  // created() {
+  //   console.log('created');
+  // },
+  // mounted() {
+  //   console.log('mounted');
+  // }
+}
+</script>
