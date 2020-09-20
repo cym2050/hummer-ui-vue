376098cd36014b872eb8fb2ff0f08309334f0226
cym2050 <1390062269@qq.com>
2020-09-20 19:42:09 +0800

    优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -5,7 +5,7 @@
       class="hummer-tabs-nav-item" 
       :class="{ selected: t === selected }" 
       v-for="(t, index) in titles" 
-      :ref="el => { if (el) navItems[index] = el }"
+      :ref="el => { if ( t === selected ) selectedItem = el }"
       :key='index'
       @click="select(t)"
      > 
@@ -29,16 +29,20 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    // :ref="el => { if (el) navItems[index] = el }"
+    // const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      const { width } = result.getBoundingClientRect()
+      // const divs = navItems.value
+      // const result = divs.filter(div => div.classList.contains('selected'))[0]
+      // const { width } = result.getBoundingClientRect()
+
+      const { width } = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = result.getBoundingClientRect()
+      const { left: left2 } = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -60,7 +64,7 @@ export default {
       context.emit('update:selected', title)
     }
 
-    return { defaults, titles, select, current, navItems, indicator, container }
+    return { defaults, titles, select, current, indicator, container, selectedItem }
   }
 }
 </script>
