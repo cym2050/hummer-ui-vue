dfc2df821463943e914e93868b1b8bd54616e306
cym2050 <1390062269@qq.com>
2020-09-21 18:07:56 +0800

    支持markdown文件

---
+++ src/markdown/intro.md
@@ -0,0 +1,9 @@
+# 介绍
+
+hummer UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。
+
+这个组件库是为实践 Vue3 与 typescript，锤炼技术能力而写，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也都是我自己写的。
+
+所以强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了 https://github.com/cym2050/hummer-ui-vue
+
+下一节：[安装](#/doc/install)
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,10 +1,16 @@
 
 <template>
-<article class="markdown-body">
-  <h1>介绍</h1>
-  <p>hummer UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-  <p>这个组件库是为实践 Vue3 与 typescript，锤炼技术能力而写，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也几乎都是我自己写的。</p>
-  <p>所以强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了 https://github.com/cym2050/hummer-ui-vue</p>
-  下一节：<a href="#/doc/install">安装</a>
+<article class="markdown-body" v-html="md">
 </article>
 </template>
+
+<script>
+import md from '../markdown/intro.md';
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
