87413d870f1edf8cc71db015d2bc2acc05e9ae42
cym2050 <1390062269@qq.com>
2020-09-25 16:13:38 +0800

    解决 build 后无法加载 md 文件的问题

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,30 @@
 import {createWebHashHistory, createRouter} from 'vue-router'
+import { h } from 'vue'
+
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-
-import { h } from 'vue';
-import Markdown from './components/Markdown.vue';
 
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 
-
+import Markdown from './components/Markdown.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 
 const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
   history: history,
   routes: [
     {path: '/', component: Home},
     {path: '/doc', component: Doc, children: [
       { path: '', redirect: '/doc/intro' },
-      { path: 'intro', component: md('Intro') },
-      { path: 'install', component: md('Install') },
-      { path: 'get-started', component: md('get-started') },
+      { path: 'intro', component: md(intro) },
+      { path: 'install', component: md(install) },
+      { path: 'get-started', component: md(getStarted) },
       { path: 'switch', component: SwitchDemo},
       { path: 'button', component: ButtonDemo},
       { path: 'dialog', component: DialogDemo},
