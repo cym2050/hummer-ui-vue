f5c12b3afe09ca3ee5660e75dfa3b8760fb5de66
cym2050 <1390062269@qq.com>
2020-10-07 16:03:04 +0800

    添加tabs展示

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,22 +1,16 @@
 <template>
   <h2>Tabs 示例</h2>
-  <h1>示例1</h1>
-  <Tabs v-model:selected='x'>
-    <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2222">内容2</Tab>
-  </Tabs>
+  <Demo :component="Tabs1demo" />
 </template>
 
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import { ref } from 'vue'
-export default {
-  components: { Tabs, Tab },
-  setup() {
-    const x = ref('导航2222')
+import Demo from './Demo.vue'
+import Tabs1demo from './Tabs1.demo.vue'
 
-    return { x }
+export default {
+  components: { Demo },
+  setup() {
+    return { Tabs1demo }
   }
 }
 </script>
